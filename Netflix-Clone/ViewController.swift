39b478656b1d0ccd2696636fde289fffc719fc6e
Anshu Vij <anshu.vij@sustlabs.com>
2022-03-05 21:29:34 +0530

    Initial Commit

---
+++ Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Anshu Vij on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
